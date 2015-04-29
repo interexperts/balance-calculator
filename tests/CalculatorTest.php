03c74b20d68696d9f640e2c73a9401929fd0a780
Geert Wirken <g.wirken@interexperts.nl>
2015-04-29 13:30:49 +0200

    Unit test voor testGetQuotumExtra

--- tests/CalculatorTest.php
+++ tests/CalculatorTest.php
@@ -6,17 +6,26 @@ use \InterExperts\BalanceCalculator\Calculator;
 class CalculatorTest extends PHPUnit_Framework_TestCase{
 
 	public function testGetQuotum(){
-		$testFiles = glob(dirname(__FILE__) . '/balance-testdata/testGetQuotum/balance-*.csv');
+		$this->doQuotumTest(dirname(__FILE__) . '/balance-testdata/testGetQuotum/');
+	}
+
+	public function testGetQuotumExtra(){
+		$this->doQuotumTest(dirname(__FILE__) . '/balance-testdata/testGetQuotumExtra/');
+	}
+
+	protected function doQuotumTest($directory, $filenamePattern = 'balance-*.csv') {
+		$testFiles = glob($directory . $filenamePattern);
+
 		foreach($testFiles as $testFile){
 			$filename = basename($testFile);
-			$fh = fopen(dirname(__FILE__). '/balance-testdata/testGetQuotum/'. $filename, 'r');
+			$fh = fopen($directory . $filename, 'r');
 			$header = fgetcsv($fh);
 			$calculator = new Calculator();
 			while($inputYear = fgetcsv($fh)){
 				$calculator->addYear(new Year(new \DateTime($inputYear[1]), $inputYear[2], $inputYear[4], $inputYear[3], $inputYear[5]));
 			}
 			fclose($fh);
-			$fh = fopen(dirname(__FILE__). '/balance-testdata/testGetQuotum/expected-'. $filename, 'r');
+			$fh = fopen($directory . 'expected-'. $filename, 'r');
 			$header = fgetcsv($fh);
 			while($testDate = fgetcsv($fh)){
 				$this->assertEquals($testDate[1], $calculator->getBalanceForDate(new \DateTime($testDate[0])), 
@@ -25,9 +34,5 @@ class CalculatorTest extends PHPUnit_Framework_TestCase{
 			}
 			fclose($fh);
 		}
-	}
-
-	public function testGetQuotumExtra(){
-		$this->assertTrue(true);
 	}
 }

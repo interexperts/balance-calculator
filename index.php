97d62dc4015d5e2fe93cc9bf9089d604977ea549
Niels de Blaauw <niels.de.blaauw@gmail.com>
2015-04-29 12:24:50 +0200

    CalculatorTest voor quotum toegevoegd

--- index.php
+++ index.php
@@ -1,25 +1,3 @@
 <?php
 
 require_once('./vendor/autoload.php');
-
-use \InterExperts\BalanceCalculator\Year;
-use \InterExperts\BalanceCalculator\Calculator;
-
-$jaar2012 = new Year(new \DateTime('2012-01-01'), 20, 6);
-$jaar2013 = new Year(new \DateTime('2013-01-01'), 20, 6);
-$jaar2014 = new Year(new \DateTime('2014-01-01'), 20, 6);
-$jaar2015 = new Year(new \DateTime('2015-01-01'), 20, 6);
-
-$berekenaar = new Calculator();
-$berekenaar->addYear($jaar2012);
-$berekenaar->addYear($jaar2013);
-$berekenaar->addYear($jaar2014);
-$berekenaar->addYear($jaar2015);
-
-$testDatums = array('2012-01-01', '2012-12-31', '2013-01-01', '2013-07-01', '2013-12-31', '2014-01-01', '2014-07-01', '2014-12-31', '2015-01-01', '2015-07-01', '2015-12-31');
-
-foreach ($testDatums as $testDatum) {
-	echo "\r\n". $testDatum. ' = ';
-	echo $berekenaar->getBalanceForDate(new DateTime($testDatum));
-}
-
--- tests/CalculatorTest.php
+++ tests/CalculatorTest.php
@@ -1,8 +1,32 @@
 <?php
 
+use \InterExperts\BalanceCalculator\Year;
+use \InterExperts\BalanceCalculator\Calculator;
+
 class CalculatorTest extends PHPUnit_Framework_TestCase{
 
-	public function testTrue(){
-		$this->assertTrue(true);
+	public function testGetQuotum(){
+		$testFiles = glob(dirname(__FILE__) . '/balance-testdata/balance-*.csv');
+		foreach($testFiles as $testFile){
+			$filename = basename($testFile);
+			$fh = fopen(dirname(__FILE__). '/balance-testdata/'. $filename, 'r');
+			$header = fgetcsv($fh);
+			$calculator = new Calculator();
+			while($inputYear = fgetcsv($fh)){
+				$calculator->addYear(new Year(new \DateTime($inputYear[1]), $inputYear[2], $inputYear[3]));
+			}
+			fclose($fh);
+			$fh = fopen(dirname(__FILE__). '/balance-testdata/expected-'. $filename, 'r');
+			$header = fgetcsv($fh);
+			while($testDate = fgetcsv($fh)){
+				$this->assertEquals($testDate[1], $calculator->getBalanceForDate(new \DateTime($testDate[0])), 
+					"
+					  Filename: {$filename}\r\n
+   					  TestDatum: {$testDate[0]}\r\n
+					"
+					);
+			}
+			fclose($fh);
+		}
 	}
 }

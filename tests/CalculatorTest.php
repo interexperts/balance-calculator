c2c7c71f9cbb5d1bd83b1aab28cc4599322b6d11
Niels de Blaauw <niels.de.blaauw@gmail.com>
2015-04-29 13:17:59 +0200

    Testfiles in een submap gestopt

--- tests/CalculatorTest.php
+++ tests/CalculatorTest.php
@@ -6,17 +6,17 @@ use \InterExperts\BalanceCalculator\Calculator;
 class CalculatorTest extends PHPUnit_Framework_TestCase{
 
 	public function testGetQuotum(){
-		$testFiles = glob(dirname(__FILE__) . '/balance-testdata/balance-*.csv');
+		$testFiles = glob(dirname(__FILE__) . '/balance-testdata/testGetQuotum/balance-*.csv');
 		foreach($testFiles as $testFile){
 			$filename = basename($testFile);
-			$fh = fopen(dirname(__FILE__). '/balance-testdata/'. $filename, 'r');
+			$fh = fopen(dirname(__FILE__). '/balance-testdata/testGetQuotum/'. $filename, 'r');
 			$header = fgetcsv($fh);
 			$calculator = new Calculator();
 			while($inputYear = fgetcsv($fh)){
 				$calculator->addYear(new Year(new \DateTime($inputYear[1]), $inputYear[2], $inputYear[3]));
 			}
 			fclose($fh);
-			$fh = fopen(dirname(__FILE__). '/balance-testdata/expected-'. $filename, 'r');
+			$fh = fopen(dirname(__FILE__). '/balance-testdata/testGetQuotum/expected-'. $filename, 'r');
 			$header = fgetcsv($fh);
 			while($testDate = fgetcsv($fh)){
 				$this->assertEquals($testDate[1], $calculator->getBalanceForDate(new \DateTime($testDate[0])), 

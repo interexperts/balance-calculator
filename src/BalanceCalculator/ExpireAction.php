f2fe0d0b87a202fd6ee5824f34506ea1a7538dca
Geert Wirken <g.wirken@interexperts.nl>
2015-04-29 15:39:16 +0200

    Comments toegevoegd

--- src/BalanceCalculator/ExpireAction.php
+++ src/BalanceCalculator/ExpireAction.php
@@ -2,13 +2,35 @@
 
 namespace InterExperts\BalanceCalculator;
 
-class ExpireAction extends Action{
+/**
+ * ExpireAction is an action for expiring balance.
+ * The main additional functionality of this object is that it
+ * calculates which UsedBalanceAction's this ExpiringBalance can account for.
+ *
+ * If this object can account for an UsedBalanceAction(), the used balance
+ * will be deducted from the balance that would originally be deducted when
+ * the quotum expires.
+ */
+class ExpireAction extends Action {
+	/**
+	 * Calculate the balance that should not be deducted because UsedBalanceAction's
+	 * already deduct this balance.
+	 *
+	 * This method inspects the $actions array and modifies the UsedBalanceAction objects
+	 * inside the array (they are flagged as 'processed' by this ExpireAction).
+	 *
+	 * @param  array &$actions Array of Action objects
+	 */
 	public function calculateExpiringBalance(&$actions){
 		$subOperation = $this->subOperation;
-		// Loop door acties van bovenliggend om huidige overblijfsels te verwijderen
+
+		// Iterate over the $actions array:
 		foreach($actions as &$action){
-			if($action->date <= $this->date){
-				if(is_a($action, '\InterExperts\BalanceCalculator\UsedBalanceAction') && is_null($action->processedBy)){
+			// Only operate on UsedBalanceAction objects which are not processed yet:
+			if (is_a($action, '\InterExperts\BalanceCalculator\UsedBalanceAction') && is_null($action->processedBy)) {
+				// Check whether the UsedBalanceAction is before the current ExpireAction:
+				if($action->date <= $this->date){
+					// Check whether the deduction would not result in negative balance:
 					if ($subOperation - $action->subOperation >= 0) {
 						$subOperation -= $action->subOperation;
 						$action->processedBy = $this;
@@ -16,7 +38,11 @@ class ExpireAction extends Action{
 				}
 			}
 		}
+
+		// Set this object to be processed by itself:
 		$this->processedBy = $this;
+
+		// New balance deduction value:
 		$this->subOperation = $subOperation;
 	}
 }

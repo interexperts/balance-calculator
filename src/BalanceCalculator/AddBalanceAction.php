6b2b1f47b34b8456593d52e20a013523e11b4074
Niels de Blaauw <n.de.blaauw@interexperts.nl>
2015-05-12 11:34:47 +0200

    expirationdate added

--- src/BalanceCalculator/AddBalanceAction.php
+++ src/BalanceCalculator/AddBalanceAction.php
@@ -2,4 +2,6 @@
 
 namespace InterExperts\BalanceCalculator;
 
-class AddBalanceAction extends Action{}
+class AddBalanceAction extends Action{
+	public $expirationDate = null; // Read-only, not used for calculations
+}
